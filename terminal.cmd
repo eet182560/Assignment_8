02b127589ad63092805e1f4f600f3b1db124d69a
eet182560 <asmitapatil2894@gmail.com>
2019-09-18 12:05:22 +0530

    Command file

---
+++ terminal.cmd
@@ -0,0 +1,148 @@
+git clone https://github.com/eet182560/hello-world.git
+ 1924  ls -lrt
+ 1925  cd hello-world/
+ 1926  ls -lrt
+ 1927  ls -la
+ 1928  git config --list
+ 1929  clear
+ 1930  git config --global user.name "eet182560"
+ 1931  git config --global user.email "asmitapatil2894@gmail.com"
+ 1932  git config --list
+ 1933  git branch
+ 1934  git branch newBr
+ 1935  git branch
+ 1936  git checkout newBr 
+ 1937  git branch
+ 1938  git checkout master 
+ 1939  ls -lrt
+ 1940  cat README.md 
+ 1941  clear
+ 1942  vim README.md 
+ 1943  git status
+ 1944  ls -ltr
+ 1945  git branch
+ 1946  git add README.md
+ 1947  git commit -m "Commit 1"
+ 1948  git log
+ 1949  git status
+ 1950  touch .gitignore
+ 1951  vim .gitignore 
+ 1952  cat .gitignore
+ 1953  git status
+ 1954  git add .gitignore
+ 1955  git status
+ 1956  rm ..gitignore.un~ .gitignore~
+ 1957  git status
+ 1958  git commit -m "Commit 2"
+ 1959  git status
+ 1960  git remote add origin https://github.com/eet182560/hello-world.git
+ 1961  git push -u origin master 
+ 1962  touch test.txt
+ 1963  git status
+ 1964  git add test.txt 
+ 1965  git status
+ 1966  git commit -m "Commit 3"
+ 1967  git log
+ 1968  git push -u origin master 
+ 1969  git pull origin master 
+ 1970  git status
+ 1971  cd ..
+ 1972  git clone https://github.com/eet182560/Spoon-Knife.git
+ 1973  clear
+ 1974  git remote -v
+ 1975  ls -lrt
+ 1976  cd Spoon-Knife/
+ 1977  ls -lrt
+ 1978  git remote -v
+ 1979  git remote add upstream https://github.com/octocat/Spoon-Knife.git
+ 1980  git remote -v
+ 1981  git fetch upstream 
+ 1982  git branch
+ 1983  git merge upstream/master 
+ 1984  exit
+ 1985  make
+ 1986  make clean
+ 1987  make ps1
+ 1988  make ps2
+ 1989  cat ouput
+ 1990  cat ouput.txt 
+ 1991  make clean
+ 1992  cat Makefile 
+ 1993  make clean
+ 1994  make ps2
+ 1995  cat output.txt 
+ 1996  make ps1
+ 1997  make clean
+ 1998  git clone https://github.com/eet182560/Assignment_8.git
+ 1999  ls -lrt
+ 2000  cd Assignment_8/
+ 2001  ls -lrt
+ 2002  cat README.md 
+ 2003  cat ps1.py 
+ 2004  cat ps2.py 
+ 2005  clear
+ 2006  ls -lrt
+ 2007  python3 ps1.py 
+ 2008  clear
+ 2009  git status
+ 2010  git add .
+ 2011  git commit -m "Initial Commit"
+ 2012  git log
+ 2013  python3 ps1.py 
+ 2014  clear
+ 2015  python3 ps1.py 
+ 2016  clear
+ 2017  python3 ps1.py 
+ 2018  clear
+ 2019  python3 ps1.py 
+ 2020  git status
+ 2021  git add .
+ 2022  git commit -m "Final version of ps1.py"
+ 2023  git log
+ 2024  git status
+ 2025  clear
+ 2026  python3 ps2.py 
+ 2027  git status
+ 2028  git add .
+ 2029  git commit -m "First commit of ps2.py"
+ 2030  git status
+ 2031  python3 ps2.py 
+ 2032  git commit -m "First commit of ps2.py"
+ 2033  python3 ps2.py 
+ 2034  git status
+ 2035  git commit -a "Commit"
+ 2036  git commit -a
+ 2037  git log
+ 2038  git add .
+ 2039  git commit -m "ps2.py"
+ 2040  ls -lrt
+ 2041  git log
+ 2042  clear
+ 2043  python3 ps2.py 
+ 2044  git status
+ 2045  git add .
+ 2046  git commit -m "Updated files"
+ 2047  git log
+ 2048  python3 ps1.py 
+ 2049  ls -lrt
+ 2050  make
+ 2051  mv input1_1.txt input2_1.txt
+ 2052  make
+ 2053  git status
+ 2054  git add .
+ 2055  git commit -m "Final Commit"
+ 2056  git status
+ 2057  git log
+ 2058  ls -lrt
+ 2059  python3 ps2.py 
+ 2060  2,8
+ 2061  5,5
+ 2062  6,4
+ 2063  python3 ps2.py 
+ 2064  python3 ps2.py < input2_2.txt 
+ 2065  python3 ps2.py
+ 2066  git status
+ 2067  git add .
+ 2068  git commit -m "Updated files"
+ 2069  git log
+ 2070  history > termina.cmd
